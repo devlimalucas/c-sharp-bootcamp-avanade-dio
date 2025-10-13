d2d8a7155be7c2d9c8a4c860313e481fbe40286e
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-13 09:16:32 -0300

    feat: criando um método

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Serialization;
+using ExemploExplorar.Models;
+
+Pessoa p1 = new();
+p1.Nome = "Lucas";
+p1.Idade = 30;
+p1.Apresentar();
