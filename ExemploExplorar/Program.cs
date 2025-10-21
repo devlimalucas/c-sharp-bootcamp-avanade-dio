e26ffef07e9336aa66d517c6f9b105a0aff16abe
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-21 12:15:55 -0300

    feat: formatação personalizada

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -5,4 +5,4 @@ CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
 decimal valorMonetario = 1580.55M;
 
-Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+Console.WriteLine(valorMonetario.ToString("C3", CultureInfo.CreateSpecificCulture("pt-BR")));
