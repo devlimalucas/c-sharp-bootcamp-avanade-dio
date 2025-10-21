bbbd26d4792e36826b068c816149cce7df0c7f5b
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-21 12:48:39 -0300

    feat: formatando o tipo DataTime

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,16 +1,6 @@
 ﻿using ExemploExplorar.Models;
 using System.Globalization;
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+DateTime data = DateTime.Now;
 
-decimal valorMonetario = 1580.55M;
-
-double porcentagem = .3421;
-
-int numero = 123456;
-
-Console.WriteLine(valorMonetario.ToString("N2"));
-
-Console.WriteLine(porcentagem.ToString("P"));
-
-Console.WriteLine(numero.ToString("##-##-##"));
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
