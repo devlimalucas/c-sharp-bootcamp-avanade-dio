a0dcc1c874a278cf2ac709e0c2286d3b2e4a8a9b
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-21 12:08:00 -0300

    feat: formatando valores monetários

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,17 +1,5 @@
 ﻿using ExemploExplorar.Models;
 
-string numero1 = "10";
-string numero2 = "20";
+decimal valorMonetario = 82.40M;
 
-int numeroInteiro1 = 10;
-int numeroInteiro2 = 20;
-
-string resultado = numero1 + numero2;
-
-int resultadoInteiros = numeroInteiro1 + numeroInteiro2;
-
-Console.WriteLine("soma numeros " + resultado);
-
-Console.WriteLine("soma numeros inteiros " + resultadoInteiros);
-
-Console.WriteLine("Dois tipos " + numero1 + numeroInteiro2);
+Console.WriteLine($"{valorMonetario:C}");
