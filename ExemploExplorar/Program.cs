91fde3674f31e6f3c08e32161cb56316906a2794
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-22 11:45:32 -0300

    feat: fila na prática

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,5 +1,25 @@
 ﻿using ExemploExplorar.Models;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 
 
-new ExemploExcecao().Metodo1();
+Queue<int> fila = new();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+fila.Enqueue(10);
+
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}
