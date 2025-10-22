b62bfd6f6ca563876eaa7481b4b8e761197d3a25
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-22 17:45:45 -0300

    feat: outra sintaxe de tupla

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -3,6 +3,10 @@ using System.Globalization;
 
 (int, string, string, decimal) tupla = (1, "Lucas", "Lima", 10.5M);
 
+ValueTuple<int, string, string, decimal> outroExemploTupla = (1, "Lucas", "Lima", 10.5M);
+
+var outroExemploTuplaCreate = Tuple.Create(1, "Lucas", "Lima", 10.5M);
+
 Console.WriteLine(tupla.Item1);
 Console.WriteLine(tupla.Item2);
 Console.WriteLine(tupla.Item3);
