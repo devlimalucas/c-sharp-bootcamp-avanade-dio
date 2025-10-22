5843ff8e39490bd1745e4060119e8159ef6e2cb9
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-22 10:37:14 -0300

    feat: usando o Throw

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,28 +1,5 @@
 ﻿using ExemploExplorar.Models;
 using System.Globalization;
 
-try
-{
-    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 
-    foreach (string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-}
-catch (FileNotFoundException ex)
-{
-    Console.WriteLine($"Ops, arquivo não encontrado: {ex.Message}");
-}
-catch (DirectoryNotFoundException ex)
-{
-    Console.WriteLine($"Ops, caminho da pasta não encontrado: {ex.Message}");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ops, ocorreu um erro: {ex.Message}");
-}
-finally
-{
-    Console.WriteLine("Chegou até aqui");
-}
+new ExemploExcecao().Metodo1();
