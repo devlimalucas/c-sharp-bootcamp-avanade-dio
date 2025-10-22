e55cb92afaca22a061aeec3ec0b260959ddf617f
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-21 22:03:17 -0300

    feat: validando retorno do TryParse

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -2,12 +2,20 @@
 using System.Data;
 using System.Globalization;
 
-string dataString = "2022-12-17 18:00";
+string dataString = "2022-02-170 18:00";
 
-DateTime.TryParseExact(dataString,
+bool sucesso = DateTime.TryParseExact(dataString,
                       "yyyy-MM-dd HH:mm",
                       CultureInfo.InvariantCulture,
                       DateTimeStyles.None,
                       out DateTime data);
 
-Console.WriteLine(data);
+if (sucesso)
+{
+    Console.WriteLine($"Conversão com sucesso! Data: {data}");
+
+}
+else
+{
+    Console.WriteLine($"{dataString} não é uma data válida");
+}
