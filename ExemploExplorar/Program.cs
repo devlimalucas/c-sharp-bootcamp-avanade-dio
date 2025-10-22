7730513e47063b350711333b0fcb2097116ad199
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-22 17:41:07 -0300

    feat: Tuplas na prática

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,47 +1,9 @@
 ﻿using ExemploExplorar.Models;
-using System.ComponentModel;
 using System.Globalization;
-using System.Runtime.CompilerServices;
 
+(int, string, string, decimal) tupla = (1, "Lucas", "Lima", 10.5M);
 
-Dictionary<string, string> estados = [];
-
-try
-{
-    estados.Add("SP", "São Paulo");
-    estados.Add("BA", "Bahia");
-    estados.Add("MG", "Minas Gerais");
-
-    foreach (var item in estados)
-    {
-        Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
-    }
-
-    Console.WriteLine("-----------");
-
-    estados.Remove("MG");
-    estados["SP"] = "São Paulo - valor alterado";
-
-    foreach (var item in estados)
-    {
-        Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
-    }
-
-    Console.WriteLine("-----------");
-
-    string chave = "BA";
-
-    if (estados.ContainsKey(chave))
-    {
-        Console.WriteLine($"O valor existente: {chave}");
-    }
-    else
-    {
-        Console.WriteLine($"Valor não existente. É seguro adicionar a chave: {chave}");
-    }
-
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ops, ocorreu um erro: {ex.Message}");
-}
+Console.WriteLine(tupla.Item1);
+Console.WriteLine(tupla.Item2);
+Console.WriteLine(tupla.Item3);
+Console.WriteLine(tupla.Item4);
