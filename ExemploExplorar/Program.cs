0e60d9c04c963d9754289bc907024536b44c77c2
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-22 09:48:29 -0300

    feat: realizando a leitura de um arquivo

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -1,21 +1,10 @@
 ﻿using ExemploExplorar.Models;
-using System.Data;
 using System.Globalization;
 
-string dataString = "2022-02-170 18:00";
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 
-bool sucesso = DateTime.TryParseExact(dataString,
-                      "yyyy-MM-dd HH:mm",
-                      CultureInfo.InvariantCulture,
-                      DateTimeStyles.None,
-                      out DateTime data);
 
-if (sucesso)
+foreach (string linha in linhas)
 {
-    Console.WriteLine($"Conversão com sucesso! Data: {data}");
-
-}
-else
-{
-    Console.WriteLine($"{dataString} não é uma data válida");
+    Console.WriteLine(linha);
 }
