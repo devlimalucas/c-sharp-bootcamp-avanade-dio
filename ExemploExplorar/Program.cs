670d50eacbbbe05132c199c41ff2bf9ae15fa14a
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-22 10:11:46 -0300

    feat: exceção genérica e específica

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -3,13 +3,22 @@ using System.Globalization;
 
 try
 {
-    string[] linhas = File.ReadAllLines("Arquivosa/arquivoLeitura.txt");
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeiturra.txt");
 
     foreach (string linha in linhas)
     {
         Console.WriteLine(linha);
     }
-} catch(Exception ex)
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Ops, arquivo não encontrado: {ex.Message}");
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Ops, caminho da pasta não encontrado: {ex.Message}");
+}
+catch (Exception ex)
 {
     Console.WriteLine($"Ops, ocorreu um erro: {ex.Message}");
 }
