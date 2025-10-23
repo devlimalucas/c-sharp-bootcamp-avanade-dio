2fc74b4be70ea416f1317ac4e717cac010ede696
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-23 10:20:05 -0300

    feat: DataTime em JSON

--- ExemploExplorar/Program.cs
+++ ExemploExplorar/Program.cs
@@ -2,10 +2,12 @@
 using System.Globalization;
 using Newtonsoft.Json;
 
+DateTime dataAtual = DateTime.Now;
+
 List<Venda> listaVendas = [];
 
-Venda v1 = new(id: 1, produto: "Material de escritório", preco: 20.30M);
-Venda v2 = new(id: 2, produto: "Licença de Software", preco: 200.56M);
+Venda v1 = new(id: 1, produto: "Material de escritório", preco: 20.30M, dataAtual);
+Venda v2 = new(id: 2, produto: "Licença de Software", preco: 200.56M, dataAtual);
 
 listaVendas.Add(v1);
 listaVendas.Add(v2);
