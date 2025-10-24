40bc7c5d4afc97af8340202498e007dd78b739d3
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-24 07:34:08 -0300

    feat: polimorfismo em tempo de execução

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -3,14 +3,19 @@ using System.Runtime.Serialization;
 using ExemploPOO.Models;
 
 Aluno a1 = new();
-
 a1.Nome = "Lucas Lima";
 a1.Idade = 30;
 
 Aluno a2 = new();
-
 a2.Nome = "Kleber Filho";
 a2.Idade = 40;
 
 a1.Apresentar();
 a2.Apresentar();
+
+Professor p1 = new();
+p1.Nome = "Lúcia";
+p1.Idade = 35;
+p1.Salario = 3000;
+
+p1.Apresentar();
