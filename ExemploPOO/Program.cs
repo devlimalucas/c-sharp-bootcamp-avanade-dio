e5c1bb5cd6ad3aca23ecf6cdde01ede5da239ef8
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-24 09:13:04 -0300

    feat: classe abstrata na prática

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -2,20 +2,7 @@
 using System.Runtime.Serialization;
 using ExemploPOO.Models;
 
-Aluno a1 = new();
-a1.Nome = "Lucas Lima";
-a1.Idade = 30;
+Corrente c = new();
 
-Aluno a2 = new();
-a2.Nome = "Kleber Filho";
-a2.Idade = 40;
-
-a1.Apresentar();
-a2.Apresentar();
-
-Professor p1 = new();
-p1.Nome = "Lúcia";
-p1.Idade = 35;
-p1.Salario = 3000;
-
-p1.Apresentar();
+c.Creditar(500);
+c.ExibirSaldo();
