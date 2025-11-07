3958d20c35e7e20c74eae049a86630bd37fb2788
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-11-07 17:59:48 -0300

    feat: criando o endpoint obter por ID

--- ModuloAPI/Controllers/ContatoController.cs
+++ ModuloAPI/Controllers/ContatoController.cs
@@ -18,12 +18,22 @@ namespace ModuloAPI.Controllers
         {
             _context = context;
         }
-        
+
         [HttpPost]
         public IActionResult Create(Contato contato)
         {
             _context.Add(contato);
             _context.SaveChanges();
+            return Ok(contato);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null) return NotFound();
+
             return Ok(contato);
         }
     }
